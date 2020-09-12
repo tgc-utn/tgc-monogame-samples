43d399c711f39cc5bbe88b27e77b39cefdc076fb
Ronan <avinitzca@gmail.com>
2020-09-12 16:00:11 -0300

    Fixed sample and added pyramid sample

---
+++ TGC.MonoGame.Samples/Samples/Physics/BoxPyramidSample.cs
@@ -0,0 +1,360 @@
+﻿using BepuPhysics;
+using BepuPhysics.Collidables;
+using BepuPhysics.CollisionDetection;
+using BepuPhysics.Constraints;
+using BepuUtilities.Memory;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using TGC.MonoGame.Samples.Cameras;
+using TGC.MonoGame.Samples.Geometries;
+using TGC.MonoGame.Samples.Geometries.Textures;
+using TGC.MonoGame.Samples.Physics;
+using TGC.MonoGame.Samples.Viewer;
+
+namespace TGC.MonoGame.Samples.Samples.Physics
+{
+    public class BoxPyramidSample : TGCSample
+    {
+        /// <summary>
+        /// Gets the simulation created by the demo's Initialize call.
+        /// </summary>
+        public Simulation Simulation { get; protected set; }
+
+        //Note that the buffer pool used by the simulation is not considered to be *owned* by the simulation. The simulation merely uses the pool.
+        //Disposing the simulation will not dispose or clear the buffer pool.
+        /// <summary>
+        /// Gets the buffer pool used by the demo's simulation.
+        /// </summary>
+        public BufferPool BufferPool { get; private set; }
+
+        /// <summary>
+        /// Gets the thread dispatcher available for use by the simulation.
+        /// </summary>
+        public SimpleThreadDispatcher ThreadDispatcher { get; private set; }
+
+        private CubePrimitive cubePrimitive;
+
+        private Camera Camera;
+
+        private List<BodyHandle> BoxHandles;
+
+        private List<Matrix> ActiveBoxesWorld;
+
+        private List<Matrix> InactiveBoxesWorld;
+
+        private List<Matrix> SpheresWorld;
+
+        private bool CanShoot = true;
+
+        private Random Random;
+
+        private List<float> Radii;
+
+        private List<BodyHandle> SphereHandles;
+
+        private SpherePrimitive spherePrimitive;
+
+        public BoxPyramidSample(TGCViewer game) : base(game)
+        {
+            Category = TGCSampleCategory.Physics;
+            Name = "BepuPhysics 3 - Boxes demo";
+            Description = "Spawning boxes dinamically.";
+        }
+
+        public override void Initialize()
+        {
+
+            BufferPool = new BufferPool();
+            //Generally, shoving as many threads as possible into the simulation won't produce the best results on systems with multiple logical cores per physical core.
+            //Environment.ProcessorCount reports logical core count only, so we'll use a simple heuristic here- it'll leave one or two logical cores idle.
+            //For the common Intel quad core with hyperthreading, this'll use six logical cores and leave two logical cores free to be used for other stuff.
+            //This is by no means perfect. To maximize performance, you'll need to profile your simulation and target hardware.
+            //Note that issues can be magnified on older operating systems like Windows 7 if all logical cores are given work.
+
+            //Generally, the more memory bandwidth you have relative to CPU compute throughput, and the more collision detection heavy the simulation is relative to solving,
+            //the more benefit you get out of SMT/hyperthreading. 
+            //For example, if you're using the 64 core quad memory channel AMD 3990x on a scene composed of thousands of ragdolls, 
+            //there won't be enough memory bandwidth to even feed half the physical cores. Using all 128 logical cores would just add overhead.
+
+            //It may be worth using something like hwloc to extract extra information to reason about.
+            var targetThreadCount = Math.Max(1, Environment.ProcessorCount > 4 ? Environment.ProcessorCount - 2 : Environment.ProcessorCount - 1);
+            ThreadDispatcher = new SimpleThreadDispatcher(targetThreadCount);
+            
+            
+
+            var size = GraphicsDevice.Viewport.Bounds.Size;
+            size.X /= 2;
+            size.Y /= 2;
+            Camera = new FreeCamera(GraphicsDevice.Viewport.AspectRatio, new Vector3(0, 50, 50), size);
+
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            Simulation = Simulation.Create(BufferPool, new NarrowPhase(), new DemoPoseIntegratorCallbacks(new System.Numerics.Vector3(0, -10, 0)), new PositionFirstTimestepper());
+
+            SphereHandles = new List<BodyHandle>();
+            ActiveBoxesWorld = new List<Matrix>();
+            InactiveBoxesWorld = new List<Matrix>();
+            SpheresWorld = new List<Matrix>();
+            Random = new Random();
+            BoxHandles = new List<BodyHandle>(800);
+            Radii = new List<float>();
+
+            var boxShape = new Box(1, 1, 1);
+            boxShape.ComputeInertia(1, out var boxInertia);
+            var boxIndex = Simulation.Shapes.Add(boxShape);
+            const int pyramidCount = 40;
+            for (int pyramidIndex = 0; pyramidIndex < pyramidCount; ++pyramidIndex)
+            {
+                const int rowCount = 20;
+                for (int rowIndex = 0; rowIndex < rowCount; ++rowIndex)
+                {
+                    int columnCount = rowCount - rowIndex;
+                    for (int columnIndex = 0; columnIndex < columnCount; ++columnIndex)
+                    {
+                        var bh = Simulation.Bodies.Add(BodyDescription.CreateDynamic(new System.Numerics.Vector3(
+                            (-columnCount * 0.5f + columnIndex) * boxShape.Width,
+                            (rowIndex + 0.5f) * boxShape.Height,
+                            (pyramidIndex - pyramidCount * 0.5f) * (boxShape.Length + 4)),
+                            boxInertia,
+                            new CollidableDescription(boxIndex, 0.1f),
+                            new BodyActivityDescription(0.01f)));
+                        BoxHandles.Add(bh);
+                    }
+                }
+            }
+
+
+            //Prevent the boxes from falling into the void.
+            Simulation.Statics.Add(new StaticDescription(new System.Numerics.Vector3(0, -0.5f, 0), new CollidableDescription(Simulation.Shapes.Add(new Box(2500, 1, 2500)), 0.1f)));
+
+            cubePrimitive = new CubePrimitive(GraphicsDevice, 1f, Color.White);
+
+            spherePrimitive = new SpherePrimitive(GraphicsDevice, 1f, 16);
+
+            int count = BoxHandles.Count;
+            GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+            for (int index = 0; index < count; index++)
+            {
+                var bodyHandle = BoxHandles[index];
+                var bodyReference = Simulation.Bodies.GetBodyReference(bodyHandle);
+                System.Numerics.Vector3 position = bodyReference.Pose.Position;
+                System.Numerics.Quaternion quaternion = bodyReference.Pose.Orientation;
+                Matrix world =
+                                Matrix.CreateFromQuaternion(new Quaternion(quaternion.X, quaternion.Y, quaternion.Z,
+                                    quaternion.W)) *
+                                Matrix.CreateTranslation(new Vector3(position.X, position.Y, position.Z));
+
+                cubePrimitive.Draw(world, Camera.View, Camera.Projection);
+            }
+
+
+
+
+            base.LoadContent();
+        }
+
+
+        public override void Update(GameTime gameTime)
+        {
+            //const float simulationDt = 1 / 60f;
+            //Using a fixed time per update to match the demos simulation update rate.
+            //time += 1 / 60f;
+            //In the demos, we use one time step per frame. We don't bother modifying the physics time step duration for different monitors so different refresh rates
+            //change the rate of simulation. This doesn't actually change the result of the simulation, though, and the simplicity is a good fit for the demos.
+            //In the context of a 'real' application, you could instead use a time accumulator to take time steps of fixed length as needed, or
+            //fully decouple simulation and rendering rates across different threads.
+            //(In either case, you'd also want to interpolate or extrapolate simulation results during rendering for smoothness.)
+            //Note that taking steps of variable length can reduce stability. Gradual or one-off changes can work reasonably well.
+            Simulation.Timestep(1 / 60f, ThreadDispatcher);
+
+
+
+            if (Game.CurrentKeyboardState.IsKeyDown(Keys.Z) && CanShoot)
+            {
+                CanShoot = false;
+                //Create the shape that we'll launch at the pyramids when the user presses a button.
+                var radius = 0.5f + 5 * (float)Random.NextDouble();
+                var bulletShape = new Sphere(radius);
+
+                //Note that the use of radius^3 for mass can produce some pretty serious mass ratios. 
+                //Observe what happens when a large ball sits on top of a few boxes with a fraction of the mass-
+                //the collision appears much squishier and less stable. For most games, if you want to maintain rigidity, you'll want to use some combination of:
+                //1) Limit the ratio of heavy object masses to light object masses when those heavy objects depend on the light objects.
+                //2) Use a shorter timestep duration and update more frequently.
+                //3) Use a greater number of solver iterations.
+                //#2 and #3 can become very expensive. In pathological cases, it can end up slower than using a quality-focused solver for the same simulation.
+                //Unfortunately, at the moment, bepuphysics v2 does not contain any alternative solvers, so if you can't afford to brute force the the problem away,
+                //the best solution is to cheat as much as possible to avoid the corner cases.
+                Vector3 velocity = Camera.FrontDirection * 30f;
+                var position = new System.Numerics.Vector3(Camera.Position.X, Camera.Position.Y, Camera.Position.Z);
+                var bodyDescription = BodyDescription.CreateConvexDynamic(position,
+                    new BodyVelocity(new System.Numerics.Vector3(velocity.X, velocity.Y, velocity.Z)),
+                    bulletShape.Radius * bulletShape.Radius * bulletShape.Radius, Simulation.Shapes, bulletShape);
+
+                var bodyHandle = Simulation.Bodies.Add(bodyDescription);
+
+                Radii.Add(radius);
+                SphereHandles.Add(bodyHandle);
+            }
+
+            if (Game.CurrentKeyboardState.IsKeyUp(Keys.Z)) CanShoot = true;
+
+            ActiveBoxesWorld.Clear();
+            InactiveBoxesWorld.Clear();
+            int count = BoxHandles.Count;
+            for (int index = 0; index < count; index++)
+            {
+                var bodyHandle = BoxHandles[index];
+                var bodyReference = Simulation.Bodies.GetBodyReference(bodyHandle);
+                System.Numerics.Vector3 position = bodyReference.Pose.Position;
+                System.Numerics.Quaternion quaternion = bodyReference.Pose.Orientation;
+                Matrix world =
+                                Matrix.CreateFromQuaternion(new Quaternion(quaternion.X, quaternion.Y, quaternion.Z,
+                                    quaternion.W)) *
+                                Matrix.CreateTranslation(new Vector3(position.X, position.Y, position.Z));
+
+                if(bodyReference.Awake)
+                    ActiveBoxesWorld.Add(world);
+                else
+                    InactiveBoxesWorld.Add(world);
+            }
+
+            SpheresWorld.Clear();
+            int sphereCount = SphereHandles.Count;
+            for (int index = 0; index < sphereCount; index++)
+            {
+                var bodyHandle = SphereHandles[index];
+                var bodyReference = Simulation.Bodies.GetBodyReference(bodyHandle);
+                System.Numerics.Vector3 position = bodyReference.Pose.Position;
+                System.Numerics.Quaternion quaternion = bodyReference.Pose.Orientation;
+                Matrix world =
+                                Matrix.CreateFromQuaternion(new Quaternion(quaternion.X, quaternion.Y, quaternion.Z,
+                                    quaternion.W)) *
+                                Matrix.CreateTranslation(new Vector3(position.X, position.Y, position.Z));
+                SpheresWorld.Add(world);
+            }
+
+            Camera.Update(gameTime);
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            int count = BoxHandles.Count;
+            GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+
+            cubePrimitive.Effect.DiffuseColor = new Vector3(1f, 0f, 0f);
+            ActiveBoxesWorld.ForEach(boxWorld => cubePrimitive.Draw(boxWorld, Camera.View, Camera.Projection));
+            cubePrimitive.Effect.DiffuseColor = new Vector3(0.1f, 0.1f, 0.3f);
+            InactiveBoxesWorld.ForEach(boxWorld => cubePrimitive.Draw(boxWorld, Camera.View, Camera.Projection));
+
+            SpheresWorld.ForEach(sphereWorld => spherePrimitive.Draw(sphereWorld, Camera.View, Camera.Projection));
+
+            base.Draw(gameTime);
+        }
+
+        protected override void UnloadContent()
+        {
+            Simulation.Dispose();
+            base.UnloadContent();
+        }
+    }
+
+
+    struct NarrowPhase : INarrowPhaseCallbacks
+    {
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool AllowContactGeneration(int workerIndex, CollidableReference a, CollidableReference b)
+        {
+            return a.Mobility == CollidableMobility.Dynamic || b.Mobility == CollidableMobility.Dynamic;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool AllowContactGeneration(int workerIndex, CollidablePair pair, int childIndexA, int childIndexB)
+        {
+            return true;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public unsafe bool ConfigureContactManifold<TManifold>(int workerIndex, CollidablePair pair, ref TManifold manifold, out PairMaterialProperties pairMaterial) where TManifold : struct, IContactManifold<TManifold>
+        {
+            pairMaterial = new PairMaterialProperties { FrictionCoefficient = 1, MaximumRecoveryVelocity = 2, SpringSettings = new SpringSettings(30, 1) };
+            return true;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public unsafe bool ConfigureContactManifold(int workerIndex, CollidablePair pair, int childIndexA, int childIndexB, ref ConvexContactManifold manifold)
+        {
+            return true;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Initialize(Simulation simulation)
+        {
+        }
+    }
+
+
+    public struct DemoPoseIntegratorCallbacks : IPoseIntegratorCallbacks
+    {
+        public System.Numerics.Vector3 Gravity;
+        public float LinearDamping;
+        public float AngularDamping;
+        System.Numerics.Vector3 gravityDt;
+        float linearDampingDt;
+        float angularDampingDt;
+
+        public AngularIntegrationMode AngularIntegrationMode => AngularIntegrationMode.Nonconserving;
+
+        public DemoPoseIntegratorCallbacks(System.Numerics.Vector3 gravity, float linearDamping = .03f, float angularDamping = .03f) : this()
+        {
+            Gravity = gravity;
+            LinearDamping = linearDamping;
+            AngularDamping = angularDamping;
+        }
+
+        public void PrepareForIntegration(float dt)
+        {
+            //No reason to recalculate gravity * dt for every body; just cache it ahead of time.
+            gravityDt = Gravity * dt;
+            //Since this doesn't use per-body damping, we can precalculate everything.
+            linearDampingDt = MathF.Pow(MathHelper.Clamp(1 - LinearDamping, 0, 1), dt);
+            angularDampingDt = MathF.Pow(MathHelper.Clamp(1 - AngularDamping, 0, 1), dt);
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void IntegrateVelocity(int bodyIndex, in RigidPose pose, in BodyInertia localInertia, int workerIndex, ref BodyVelocity velocity)
+        {
+            //Note that we avoid accelerating kinematics. Kinematics are any body with an inverse mass of zero (so a mass of ~infinity). No force can move them.
+            if (localInertia.InverseMass > 0)
+            {
+                velocity.Linear = (velocity.Linear + gravityDt) * linearDampingDt;
+                velocity.Angular = velocity.Angular * angularDampingDt;
+            }
+            //Implementation sidenote: Why aren't kinematics all bundled together separately from dynamics to avoid this per-body condition?
+            //Because kinematics can have a velocity- that is what distinguishes them from a static object. The solver must read velocities of all bodies involved in a constraint.
+            //Under ideal conditions, those bodies will be near in memory to increase the chances of a cache hit. If kinematics are separately bundled, the the number of cache
+            //misses necessarily increases. Slowing down the solver in order to speed up the pose integrator is a really, really bad trade, especially when the benefit is a few ALU ops.
+
+            //Note that you CAN technically modify the pose in IntegrateVelocity by directly accessing it through the Simulation.Bodies.ActiveSet.Poses, it just requires a little care and isn't directly exposed.
+            //If the PositionFirstTimestepper is being used, then the pose integrator has already integrated the pose.
+            //If the PositionLastTimestepper or SubsteppingTimestepper are in use, the pose has not yet been integrated.
+            //If your pose modification depends on the order of integration, you'll want to take this into account.
+
+            //This is also a handy spot to implement things like position dependent gravity or per-body damping.
+        }
+
+    }
+}
