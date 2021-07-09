7131144cfed0a55c5f63e740dc2467a6df34e5ee
avinitzca <avinitzca@gmail.com>
2021-07-09 19:57:10 -0300

    First iteration on post-processing effect

---
+++ TGC.MonoGame.Samples/Content/Effects/ChromaticAberration.fx
@@ -0,0 +1,156 @@
+﻿#if OPENGL
+#define SV_POSITION POSITION
+#define VS_SHADERMODEL vs_3_0
+#define PS_SHADERMODEL ps_3_0
+#else
+#define VS_SHADERMODEL vs_4_0_level_9_1
+#define PS_SHADERMODEL ps_4_0_level_9_1
+#endif
+
+uniform float2 ScreenDelta;
+uniform float Displacement;
+
+uniform texture BaseTexture;
+uniform sampler2D textureSampler = sampler_state
+{
+    Texture = (BaseTexture);
+    MagFilter = Linear;
+    MinFilter = Linear;
+    AddressU = Clamp;
+    AddressV = Clamp;
+};
+
+    
+struct VertexShaderInput
+{
+    float4 Position : POSITION0;
+    float2 TextureCoordinates : TEXCOORD0;
+};
+
+struct VertexShaderOutput
+{
+    float4 Position : SV_POSITION;
+    float2 TextureCoordinates : TEXCOORD0;
+};
+
+
+VertexShaderOutput MainVS(in VertexShaderInput input)
+{
+    VertexShaderOutput output = (VertexShaderOutput) 0;
+
+    output.Position = input.Position;
+    output.TextureCoordinates = input.TextureCoordinates;
+	
+    return output;
+}
+
+float4 BasicChromaticPS(VertexShaderOutput input) : COLOR
+{
+    // Sample the base color in the Render Target
+    float4 baseColor = tex2D(textureSampler, input.TextureCoordinates);
+    
+    // Set our delta as a displacement in X, a given set of pixels
+    // ScreenDelta is a vector containing (1.0 / ScreenWidth, 1.0 / ScreenHeight)
+    float2 delta = float2(ScreenDelta.x * Displacement, 0.0);
+    
+    // Sample from our point but N pixels to the Right
+    float4 sampleRight = tex2D(textureSampler, input.TextureCoordinates + delta);
+    
+    // Sample from our point but N pixels to the Left
+    float4 sampleLeft = tex2D(textureSampler, input.TextureCoordinates - delta);
+    
+    // Use the Red Channel from the displaced Right sample
+    // Use the Green Channel from the Render Target sample
+    // Use the Blue Channel from the displaced Left sample
+    float3 color = float3(sampleRight.r, baseColor.g, sampleLeft.b);
+    
+    return float4(color, 1.0);
+}
+
+float4 CenteredChromaticPS(VertexShaderOutput input) : COLOR
+{
+    // Sample the base color in the Render Target
+    float4 baseColor = tex2D(textureSampler, input.TextureCoordinates);
+    
+    // Get the distance from our coordinates to the center of the screen
+    float distanceToCenter = distance(input.TextureCoordinates, float2(0.5, 0.5));
+    
+    // Set our delta as a displacement in X, a given set of pixels
+    // ScreenDelta is a vector containing (1.0 / ScreenWidth, 1.0 / ScreenHeight)
+    // Also, scale it by the distance to the center of the screen
+    float2 delta = float2(ScreenDelta.x * Displacement * distanceToCenter, 0.0);
+    
+    // Sample from our point but N pixels to the Right
+    float4 sampleRight = tex2D(textureSampler, input.TextureCoordinates + delta);
+    
+    // Sample from our point but N pixels to the Left
+    float4 sampleLeft = tex2D(textureSampler, input.TextureCoordinates - delta);
+    
+    // Use the Red Channel from the displaced Right sample
+    // Use the Green Channel from the Render Target sample
+    // Use the Blue Channel from the displaced Left sample
+    float3 color = float3(sampleRight.r, baseColor.g, sampleLeft.b);
+    
+    return float4(color, 1.0);
+}
+
+float4 CenteredDirectionalChromaticPS(VertexShaderOutput input) : COLOR
+{
+    // Sample the base color in the Render Target
+    float4 baseColor = tex2D(textureSampler, input.TextureCoordinates);
+    
+    // Get coordinates where (0, 0) is the center of the screen
+    // This is to use the coordinates as vector for intensity and direction of Chromatic samples
+    float2 centeredCoordinates = input.TextureCoordinates * 2.0 - 1.0;
+    
+    // Set our delta as a displacement in X, a given set of UNITS
+    // As our vector is a circular direction and is not bound to Width and Height, 
+    // We use the minimum between Height and Width to scale, as the coordinate direction is way too strong
+    float2 delta = centeredCoordinates * Displacement * min(ScreenDelta.x, ScreenDelta.y);
+    
+    // Sample from our point but N units away from the center of the screen
+    float4 sampleAway = tex2D(textureSampler, input.TextureCoordinates + delta);
+    
+    // Sample from our point but N units towards to the center of the screen
+    float4 sampleTowards = tex2D(textureSampler, input.TextureCoordinates - delta);
+    
+    // Use the Red Channel from the displaced Away sample
+    // Use the Green Channel from the Render Target sample
+    // Use the Blue Channel from the displaced Towards sample
+    float3 color = float3(sampleAway.r, baseColor.g, sampleTowards.b);
+    
+    return float4(color, 1.0);
+}
+
+
+technique BasicChromaticAberration
+{
+    pass Pass0
+    {
+        VertexShader = compile VS_SHADERMODEL MainVS();
+        PixelShader = compile PS_SHADERMODEL BasicChromaticPS();
+    }
+};
+
+
+technique CenteredChromaticAberration
+{
+    pass Pass0
+    {
+        VertexShader = compile VS_SHADERMODEL MainVS();
+        PixelShader = compile PS_SHADERMODEL CenteredChromaticPS();
+    }
+};
+
+
+technique CenteredDirectionalChromaticAberration
+{
+    pass Pass0
+    {
+        VertexShader = compile VS_SHADERMODEL MainVS();
+        PixelShader = compile PS_SHADERMODEL CenteredDirectionalChromaticPS();
+    }
+};
+
+
+
---
+++ TGC.MonoGame.Samples/Samples/PostProcessing/ChromaticAberration.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TGC.MonoGame.Samples.Cameras;
+using TGC.MonoGame.Samples.Geometries;
+using TGC.MonoGame.Samples.Viewer;
+using TGC.MonoGame.Samples.Viewer.GUI.Modifiers;
+
+namespace TGC.MonoGame.Samples.Samples.PostProcessing
+{
+    public class ChromaticAberration : TGCSample
+    {
+        private FreeCamera Camera { get; set; }
+
+        private Model Model { get; set; }
+
+        private Effect Effect { get; set; }
+
+        private FullScreenQuad FullScreenQuad { get; set; }
+
+        private RenderTarget2D SceneRenderTarget { get; set; }
+
+
+        /// <inheritdoc />
+        public ChromaticAberration(TGCViewer game) : base(game)
+        {
+            Category = TGCSampleCategory.PostProcessing;
+            Name = "Chromatic Aberration";
+            Description = "Chromatic Aberration post-processing using a Render Target";
+        }
+
+
+        /// <inheritdoc />
+        public override void Initialize()
+        {
+            var screenSize = new Point(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2);
+            Camera = new FreeCamera(GraphicsDevice.Viewport.AspectRatio, new Vector3(-300, 100, 0), screenSize);
+
+
+            base.Initialize();
+        }
+
+        /// <inheritdoc />
+        protected override void LoadContent()
+        {
+            // We load the city meshes into a model
+            Model = Game.Content.Load<Model>(ContentFolder3D + "scene/city");
+
+            // Load the Chromatic Aberration effect
+            Effect = Game.Content.Load<Effect>(ContentFolderEffects + "ChromaticAberration");
+            Effect.Parameters["ScreenDelta"].SetValue(new Vector2(1f / GraphicsDevice.Viewport.Width, 1f / GraphicsDevice.Viewport.Height));
+
+            // Create a full screen quad to post-process
+            FullScreenQuad = new FullScreenQuad(GraphicsDevice);
+
+            // Create a render target for the scene
+            SceneRenderTarget = new RenderTarget2D(GraphicsDevice, GraphicsDevice.Viewport.Width,
+                GraphicsDevice.Viewport.Height, false, SurfaceFormat.Color, DepthFormat.Depth24, 0,
+                RenderTargetUsage.DiscardContents);
+
+            GraphicsDevice.BlendState = BlendState.Opaque;
+
+            Modifiers = new IModifier[]
+            {
+                new OptionsModifier("Chromatic Aberration Type", new string[]{"Basic", "Centered", "Centered Directional" }, 0, OnChangeChromaticType),
+                new FloatModifier("Displacement", Effect.Parameters["Displacement"], 1f, 0f, 30f),
+            };
+            
+            base.LoadContent();
+        }
+
+        /// <summary>
+        ///     Processes a change in the Chromatic Aberration Type, setting the right Technique in the Shader
+        /// </summary>
+        /// <param name="index">The index of the option</param>
+        /// <param name="name">The name of the option</param>
+        private void OnChangeChromaticType(int index, string name)
+        {
+            switch(index)
+            {
+                case 0:
+                    Effect.CurrentTechnique = Effect.Techniques["BasicChromaticAberration"];
+                    break;
+                case 1:
+                    Effect.CurrentTechnique = Effect.Techniques["CenteredChromaticAberration"];
+                    break;
+                case 2:
+                    Effect.CurrentTechnique = Effect.Techniques["CenteredDirectionalChromaticAberration"];
+                    break;
+            }
+        }
+
+        /// <inheritdoc />
+        public override void Update(GameTime gameTime)
+        {
+            // Update the state of the camera
+            Camera.Update(gameTime);
+
+            Game.Gizmos.UpdateViewProjection(Camera.View, Camera.Projection);
+
+            base.Update(gameTime);
+        }
+
+        /// <inheritdoc />
+        public override void Draw(GameTime gameTime)
+        {
+            #region Pass 1
+
+            GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+            // Set the render target, we are drawing into this texture
+            GraphicsDevice.SetRenderTarget(SceneRenderTarget);
+            GraphicsDevice.Clear(ClearOptions.Target | ClearOptions.DepthBuffer, Color.CornflowerBlue, 1f, 0);
+
+            // Draw the scene to the Render Target
+            Model.Draw(Matrix.Identity, Camera.View, Camera.Projection);
+
+            #endregion
+
+            #region Pass 2
+
+            // No depth needed
+            GraphicsDevice.DepthStencilState = DepthStencilState.None;
+            // Set the render target to null, we are drawing to the screen
+            GraphicsDevice.SetRenderTarget(null);
+
+            // Draw the Full Screen Quad with our Post-Process Effect
+            Effect.Parameters["BaseTexture"].SetValue(SceneRenderTarget);
+            FullScreenQuad.Draw(Effect);
+
+            #endregion
+
+
+            base.Draw(gameTime);
+        }
+
+        protected override void UnloadContent()
+        {
+            base.UnloadContent();
+            FullScreenQuad.Dispose();
+            SceneRenderTarget.Dispose();
+        }
+    }
+}
