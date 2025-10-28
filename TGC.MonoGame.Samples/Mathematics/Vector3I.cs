60bae0e92cd6edd5984a4e265ccbb904ce53490d
AVinitzca <AVinitzca@gmail.com>
2025-10-27 22:24:26 -0300

    Initial sample

---
+++ TGC.MonoGame.Samples/Mathematics/Vector3I.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace TGC.MonoGame.Samples.Mathematics;
+
+/// <summary>
+/// Describes a 3D-vector on integer space.
+/// </summary>
+public struct Vector3I : IEquatable<Vector3I>
+{
+    /// <summary>
+    /// The first component of the vector.
+    /// </summary>
+    public int X;
+        
+    /// <summary>
+    /// The second component of the vector.
+    /// </summary>
+    public int Y;
+        
+    /// <summary>
+    /// The third componentn of the vector.
+    /// </summary>
+    public int Z;
+
+    /// <summary>
+    /// Creates a vector setting each of its components to the value provided.
+    /// </summary>
+    public Vector3I(int value)
+    {
+        X = value;
+        Y = value;
+        Z = value;
+    }
+    
+    /// <summary>
+    /// Creates a vector setting each component.
+    /// </summary>
+    public Vector3I(int x, int y, int z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+        
+    /// <summary>
+    /// Converter for <see cref="Vector3"/>, casting each component to int.
+    /// </summary>
+    public static explicit operator Vector3I(Vector3 vector)
+    {
+        return new Vector3I((int)vector.X, (int)vector.Y, (int)vector.Z);
+    }
+
+    public bool Equals(Vector3I other)
+    {
+        return X == other.X && Y == other.Y && Z == other.Z;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is Vector3I other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y, Z);
+    }
+
+    public override string ToString()
+    {
+        return $"{{{X}, {Y}, {Z}}}";
+    }
+}
---
+++ TGC.MonoGame.Samples/Samples/Optimizations/SparseGrid.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using TGC.MonoGame.Samples.Cameras;
+using TGC.MonoGame.Samples.Collisions;
+using TGC.MonoGame.Samples.Mathematics;
+using TGC.MonoGame.Samples.Viewer;
+
+namespace TGC.MonoGame.Samples.Samples.Optimizations;
+
+/// <summary>
+/// Shows how to create and use a sparse grid that is updated in real-time.
+/// Author: Ronan Vinitzca.
+/// </summary>
+public class SparseGrid : TGCSample
+{
+    private const float CellSize = 1000f;
+    
+    private const float WorldSize = 50000f;
+    
+    private readonly Random _random = new();
+    
+    private List<InstanceData> _instances;
+
+    private Dictionary<Vector3I, List<int>> _grid = new();
+
+    // Camera to draw the scene
+    private Camera _camera;
+    
+    // The Model of the Robot to draw
+    private Model _robot;
+    
+    private BoundingBox _robotAABB;
+    
+    /// <summary>
+    /// A Camera to check against bounding volumes
+    /// </summary>
+    private Camera _testCamera;
+    
+    /// <summary>
+    /// A Bounding Frustum to check visibility
+    /// </summary>
+    private BoundingFrustum _boundingFrustum;
+
+    private List<int> _indicesToDraw = new();
+
+    private bool _enabled = true;
+    
+    public SparseGrid(TGCViewer game) : base(game)
+    {
+        Category = TGCSampleCategory.Optimizations;
+        Name = "Sparse Grid";
+        Description = "Shows how to create a Sparse Grid to optimize rendering";
+    }
+
+    public override void Initialize()
+    {
+        Game.Background = Color.CornflowerBlue;
+            
+        // Creates a Static Camera looking at the origin
+        _camera = new StaticCamera(GraphicsDevice.Viewport.AspectRatio, 
+            Vector3.One * 5000f, -Vector3.Normalize(Vector3.One), Vector3.Up);
+        
+        _camera.BuildProjection(GraphicsDevice.Viewport.AspectRatio, 0.1f, 1000000f,
+            (MathF.PI / 180f) * 60f);
+
+        var size = GraphicsDevice.Viewport.Bounds.Size;
+        size.X /= 2;
+        size.Y /= 2;
+
+        // Create a camera not to render objects but to test them against its frustum
+        _testCamera = new FreeCamera(GraphicsDevice.Viewport.AspectRatio, new Vector3(0, 50, 1000), size);
+        _testCamera.BuildProjection(GraphicsDevice.Viewport.AspectRatio, 0.1f, 5000f,
+            (MathF.PI / 180f) * 35f);
+
+        _boundingFrustum = new BoundingFrustum(_testCamera.View * _testCamera.Projection);
+
+        ModifierController.AddToggle("Use Sparse Grid", (toggle) => _enabled = toggle, true);
+        
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        // Load the Robot Model and enable default lighting
+        _robot = Game.Content.Load<Model>(ContentFolder3D + "tgcito-classic/tgcito-classic");
+        ((BasicEffect)_robot.Meshes.FirstOrDefault()?.Effects.FirstOrDefault())?.EnableDefaultLighting();
+        
+        _robotAABB = BoundingVolumesExtensions.CreateAABBFrom(_robot);
+        
+        _instances = new List<InstanceData>();
+
+        for (int index = 0; index < 450_000; index++)
+        {
+            _instances.Add(new InstanceData()
+            {
+                Color = new Color(new Vector3(_random.NextSingle(), 
+                    _random.NextSingle(),
+                    _random.NextSingle())),
+                Objective = GeneratePositionInRange(_random),
+                Timer = _random.NextSingle(),
+                From = GeneratePositionInRange(_random),
+            });
+
+            ref var instance = ref CollectionsMarshal.AsSpan(_instances)[index];
+            instance.Position = Vector3.Lerp(instance.From, instance.Objective, instance.Timer);
+            
+            var cell = (Vector3I)Vector3.Floor(instance.Position / CellSize);
+            instance.CurrentCell = cell;
+            instance.BoundingBox = new BoundingBox(_robotAABB.Min + instance.Position, _robotAABB.Max + instance.Position);
+
+            if (!_grid.TryGetValue(cell, out var list))
+            {
+                list = new();
+                _grid.Add(cell, list);
+            }
+
+            list.Add(index);
+        }
+        
+        // Set depth to default
+        GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+
+        base.LoadContent();
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        float elapsedTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        
+        var instanceSpan = CollectionsMarshal.AsSpan(_instances);
+        
+        // Update 100 random instances
+        MoveInstances(instanceSpan, elapsedTime);
+
+        _indicesToDraw.Clear();
+
+        _testCamera.Update(gameTime);
+        
+        _boundingFrustum.Matrix = _testCamera.View * _testCamera.Projection;
+
+        if (_enabled)
+        {
+            TraverseGridAndFindInstances();
+        }
+        else
+        {
+            // Populate indices to draw by just doing frustum-culling
+            for (int index = 0; index < _instances.Count; index++)
+            {
+                var box = _instances[index].BoundingBox;
+                
+                if (box.Intersects(_boundingFrustum))
+                {
+                    _indicesToDraw.Add(index);
+                }
+            }
+        }
+        
+        foreach(var index in _indicesToDraw)
+        {
+            var box = _instances[index].BoundingBox;
+            var center = (box.Max + box.Min) * 0.5f;
+            var extents = (box.Max - box.Min) * 0.5f;
+            
+            Game.Gizmos.DrawCube(center, extents, Color.Lime);
+        }
+        
+        // Draw a gizmo for the frustum
+        Game.Gizmos.DrawFrustum(_testCamera.View * _testCamera.Projection, Color.Yellow);
+        
+        
+        // Update Gizmos with the View Projection matrices
+        Game.Gizmos.UpdateViewProjection(_camera.View, _camera.Projection);
+        
+        base.Update(gameTime);
+    }
+
+    private void TraverseGridAndFindInstances()
+    {
+        var corners = new Vector3[8];
+        _boundingFrustum.GetCorners(corners);
+
+        BoundingBox box = new BoundingBox();
+        foreach (Vector3 corner in corners)
+        {
+            box.Min = Vector3.Min(box.Min, corner);
+            box.Max = Vector3.Max(box.Max, corner);
+        }
+
+        var min = GetCell(box.Min);
+        var max = GetCell(box.Max);
+            
+        for(int x = min.X; x <= max.X; x++)
+        for(int y = min.Y; y <= max.Y; y++)
+        for(int z = min.Z; z <= max.Z; z++)
+        {
+            var cellPositionInWorld = new Vector3(x, y, z) * CellSize;
+            
+            var bb = new BoundingBox(cellPositionInWorld, 
+                cellPositionInWorld + new Vector3(CellSize));
+
+            if (!bb.Intersects(_boundingFrustum))
+                continue;
+
+            var center = (bb.Max + bb.Min) * 0.5f;
+            var extents = bb.Max - bb.Min;
+
+            if (!_grid.TryGetValue(new Vector3I(x, y, z), out var list))
+            {
+                Game.Gizmos.DrawCube(center, extents, Color.Red);
+                continue;
+            }
+
+            Game.Gizmos.DrawCube(center, extents, Color.Green);
+                
+            foreach (int item in list)
+            {
+                if (_boundingFrustum.Intersects(_instances[item].BoundingBox))
+                {
+                    _indicesToDraw.Add(item);
+                }
+            }
+        }
+    }
+
+    private void MoveInstances(Span<InstanceData> instanceSpan, float elapsedTime)
+    {
+        for (int index = 0; index < 100; index++)
+        {
+            ref var element = ref instanceSpan[index];
+            element.Timer += elapsedTime;
+
+            if (element.Timer >= 1f)
+            {
+                element.Timer %= 1f;
+                element.From = element.Objective;
+                element.Objective = GeneratePositionInRange(_random);
+            }
+            
+            element.Position = Vector3.Lerp(element.From, element.Objective, element.Timer);
+            element.BoundingBox = new BoundingBox(_robotAABB.Min + element.Position, _robotAABB.Max + element.Position);
+
+            var cell = GetCell(element.Position);
+
+            // Instance is on the same cell
+            if (cell.Equals(element.CurrentCell))
+                continue;
+            
+            _grid[element.CurrentCell].Remove(index);
+
+            if (_grid[element.CurrentCell].Count == 0)
+            {
+                _grid.Remove(element.CurrentCell);
+            }
+
+            if (!_grid.TryGetValue(cell, out var list))
+            {
+                list = new List<int>();
+                _grid.Add(cell, list);
+            }
+                
+            list.Add(index);
+                
+            element.CurrentCell = cell;
+        }
+    }
+
+    public override void Draw(GameTime gameTime)
+    {
+        Game.GraphicsDevice.Clear(Color.CornflowerBlue);
+    
+        var instanceSpan = CollectionsMarshal.AsSpan(_instances);
+    
+        foreach (var index in _indicesToDraw)
+        {
+            ref var element = ref instanceSpan[index];
+
+            if (_robot.Meshes.FirstOrDefault() != null &&
+                _robot.Meshes.FirstOrDefault()!.Effects.FirstOrDefault() is BasicEffect basicEffect)
+            {
+                basicEffect.DiffuseColor = element.Color.ToVector3();
+            }
+            
+            var world = Matrix.CreateTranslation(element.Position);
+            
+            _robot.Draw(world, _camera.View, _camera.Projection);
+        }
+    }
+
+    private Vector3 GeneratePositionInRange(Random random)
+    {
+        return new Vector3(RandomRange(random, -WorldSize, WorldSize),
+            RandomRange(random, -WorldSize, WorldSize),
+            RandomRange(random, -WorldSize, WorldSize));
+    }
+
+    private float RandomRange(Random random, float min, float max)
+    {
+        return random.NextSingle() * (max - min) + min;
+    }
+
+    private Vector3I GetCell(in Vector3 position)
+    {
+        return (Vector3I)Vector3.Floor(position / CellSize);
+    }
+    
+    private struct InstanceData
+    {
+        public Vector3 Position;
+        public Vector3 From;
+        public Vector3 Objective;
+        public float Timer;
+        public Color Color;
+        public Vector3I CurrentCell;
+        public BoundingBox BoundingBox;
+    }
+}
