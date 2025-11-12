8eba3c73f1a0a86832d5d2dcf770b4c33638de9e
Javier <javier.rotelli@gmail.com>
2025-11-11 21:30:27 -0300

    fix codacy errors

--- TGC.MonoGame.Samples/Mathematics/Vector3I.cs
+++ TGC.MonoGame.Samples/Mathematics/Vector3I.cs
@@ -11,38 +11,65 @@ public struct Vector3I : IEquatable<Vector3I>
     /// <summary>
     /// The first component of the vector.
     /// </summary>
-    public int X;
-        
+    private int x;
+
     /// <summary>
     /// The second component of the vector.
     /// </summary>
-    public int Y;
-        
+    private int y;
+
     /// <summary>
     /// The third componentn of the vector.
     /// </summary>
-    public int Z;
+    private int z;
+
+    /// <summary>
+    /// Gets or sets the first component of the vector.
+    /// </summary>
+    public int X
+    {
+        get => x;
+        set => x = value;
+    }
+
+    /// <summary>
+    /// Gets or sets the second component of the vector.
+    /// </summary>
+    public int Y
+    {
+        get => y;
+        set => y = value;
+    }
+
+    /// <summary>
+    /// Gets or sets the third component of the vector.
+    /// </summary>
+    public int Z
+    {
+        get => z;
+        set => z = value;
+    }
 
     /// <summary>
     /// Creates a vector setting each of its components to the value provided.
     /// </summary>
     public Vector3I(int value)
     {
-        X = value;
-        Y = value;
-        Z = value;
+        x = value;
+        y = value;
+        z = value;
     }
-    
+
     /// <summary>
     /// Creates a vector setting each component.
     /// </summary>
     public Vector3I(int x, int y, int z)
     {
-        X = x;
-        Y = y;
-        Z = z;
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
-        
+
     /// <summary>
     /// Converter for <see cref="Vector3"/>, casting each component to int.
     /// </summary>
@@ -53,7 +80,7 @@ public struct Vector3I : IEquatable<Vector3I>
 
     public bool Equals(Vector3I other)
     {
-        return X == other.X && Y == other.Y && Z == other.Z;
+        return x == other.x && y == other.y && z == other.z;
     }
 
     public override bool Equals(object obj)
@@ -63,7 +90,7 @@ public struct Vector3I : IEquatable<Vector3I>
 
     public override int GetHashCode()
     {
-        return HashCode.Combine(X, Y, Z);
+        return HashCode.Combine(x, y, z);
     }
 
     public override string ToString()
